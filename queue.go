ed1739816568fda95a665bc2cea0a0a71a795b51
dongxu <huang@wandoujia.com>
2015-01-07 22:22:49 +0800

    little refactory

--- queue.go
+++ queue.go
@@ -1,6 +1,10 @@
 package jq
 
-import "errors"
+import (
+	"container/list"
+	"errors"
+	"sync"
+)
 
 var ErrEmpty = errors.New("queue is empty")
 
@@ -12,3 +16,46 @@ type Queue interface {
 }
 
 type QueueFactory func(name string) Queue
+
+type MemQ struct {
+	lst  *list.List
+	name string
+	lck  sync.Mutex
+}
+
+func (q *MemQ) Push(v []byte) error {
+	q.lck.Lock()
+	defer q.lck.Unlock()
+	q.lst.PushBack(v)
+	return nil
+}
+
+func (q *MemQ) Pop() ([]byte, error) {
+	q.lck.Lock()
+	defer q.lck.Unlock()
+
+	if q.lst.Len() == 0 {
+		return nil, ErrEmpty
+	}
+
+	v := q.lst.Front()
+	q.lst.Remove(v)
+	return v.Value.([]byte), nil
+}
+
+func (q *MemQ) Len() int {
+	q.lck.Lock()
+	q.lck.Unlock()
+	return q.lst.Len()
+}
+
+func (q *MemQ) Name() string {
+	return q.name
+}
+
+func MemQFactory(name string) Queue {
+	return &MemQ{
+		name: name,
+		lst:  list.New(),
+	}
+}
